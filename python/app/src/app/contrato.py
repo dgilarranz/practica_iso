a491f70f72151bb17f7aed3565f1cd14ac516877
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-10-03 11:02:38 +0200

    Contrato corregido => pasa todos los test

--- python/app/src/app/contrato.py
+++ python/app/src/app/contrato.py
@@ -1,4 +1,7 @@
 import web3
+from eth_account import Account
+from eth_account.signers.local import LocalAccount
+from web3.middleware import construct_sign_and_send_raw_middleware
 from app.excepciones import NoWeb3Connection
 
 class Contrato:
@@ -8,31 +11,48 @@ class Contrato:
     
     def __init__(self):
         self.owner = open('resources/owner_address.txt', 'r').read()
+        self.w3 = self.__conectar_a_testnet()
         self.contrato = self.__conectar_a_contrato()
 
-    def __conectar_a_contrato(self):
+    def __conectar_a_testnet(self):
         # Nos conectamos a la TestNet
         w3 = web3.Web3(web3.HTTPProvider(self.TEST_NET_URL))
 
         if not w3.isConnected():
             raise NoWeb3Connection
 
+        # Creamos la cuenta que se usará para las transacciones
+        cuenta = Account.from_key(open('resources/account_key.txt', 'r').read())
+        w3.middleware_onion.add(construct_sign_and_send_raw_middleware(cuenta))
+        w3.eth.default_account = cuenta
+
+        return w3
+
+
+    def __conectar_a_contrato(self):
         # Obtenemos el contrato (necesitamos abi y dirección)
         address = open('resources/direccion_contrato.txt', 'r').read()
         abi = open('resources/abi.json', 'r').read()
-
-        return w3.eth.contract(address=address, abi=abi)
+    
+        return self.w3.eth.contract(address=address, abi=abi)
     
     def actualizar_ip(self, hash_usuario, ip_cifrada):
         try:
-            self.contrato.functions.updateIp(hash_usuario, ip_cifrada).transact()
-        except Exception:
+            txn_hash = self.contrato.functions.updateIp(hash_usuario, ip_cifrada).transact(
+                {
+                    'from': self.owner
+                }
+            )
+            self.w3.eth.waitForTransactionReceipt(txn_hash)
+        except Exception as e:
+            print(e.args)
             return False
         else:
             return True
 
     def consultar_ip(self, hash_usuario):
-        return self.contrato.functions.getIp(hash_usuario).call()
+        return self.contrato.functions.getIp(hash_usuario).call({'from': self.owner}
+        )
         
 
 
--- python/app/tests/test_contrato.py
+++ python/app/tests/test_contrato.py
@@ -4,11 +4,11 @@ from app.excepciones import NoWeb3Connection
 import pytest
 
 @patch ('app.contrato.Contrato.TEST_NET_URL', 'http://url_incorrecta.com')
-def test_conectar_a_contrato_url_erronea():
+def test_conectar_a_testnet_url_incorrect():
     with pytest.raises(NoWeb3Connection):
         Contrato()
 
-def test_conectar_a_contrato():
+def test_conectar_a_testnet_url_correcta():
     address = open('resources/direccion_contrato.txt', 'r').read()
     assert Contrato().contrato.address == address
 
