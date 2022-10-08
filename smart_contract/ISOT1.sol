753bf0f69b7f03e524cd3198a6c891bfba41db05
luislazarraga <l.perezlazarraga@usp.ceu.es>
2022-10-09 00:51:32 +0200

    Bytes32 & IpPublic Mapping

--- smart_contract/ISOT1.sol
+++ smart_contract/ISOT1.sol
@@ -4,22 +4,28 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ISOApp is Ownable{
-    //El string1 sería el hash-Id   String2 sería Hash/Ip
-    mapping(string => string) public hashToIp;
-    user[] public users;
     
-    struct user{
-        string hashId;
-        string hashIp;
-    } 
+    //El string1 sería el hash-Id   String2 sería Hash/Ip
+    mapping(bytes32 => bytes32) public hashToPrivateIp;
+    mapping(bytes32 => bytes32) public hashToPublicIp;
 
-    function updateIp(string memory _idHash, string memory _ipHash) public onlyOwner{
-        hashToIp[_idHash] = _ipHash;
+    
+    function updatePrivateIp(bytes32 _idHash, bytes32 _privateIpHash) public onlyOwner{
+        hashToPrivateIp[_idHash] = _privateIpHash;
     }
 
-    function getIp(string memory _idHash) public view returns (string memory) {
-        return hashToIp[_idHash];
+    function updatePublicIp(bytes32 _idHash, bytes32 _publicIpHash) public onlyOwner{
+        hashToPublicIp[_idHash] = _publicIpHash;
     }
 
+//    Justo 32 bytes - Usar para tests "0x4554480000000000000000000000000000000000000000000000000000000000"
+
+    function getPrivateIp(bytes32 _idHash) public view returns (bytes32 ) {
+        return hashToPrivateIp[_idHash];
+    }
+
+    function getPublicIp(bytes32 _idHash) public view returns (bytes32 ) {
+        return hashToPublicIp[_idHash];
+    }
 
 }
