518ec1bb23864508845ded49a40020df0f1c030a
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-10-13 15:25:52 +0200

    Demo modificada

--- python/app/src/app/demo.py
+++ python/app/src/app/demo.py
@@ -1,30 +1,26 @@
+import asyncio
+from ssl import HAS_TLSv1
 from time import sleep
+from app.contacto import Contacto
+from app.mensaje import Mensaje
 from app.usuario import Usuario
 from app.setup import inicializar_usuario, obtener_ip, cifrar_ip, obtener_ip_privada
 from app.contrato import Contrato
+from app.cyphersuite import hash_to_string, string_to_hash, pub_key_to_string, string_to_pub_key, priv_key_to_string, string_to_priv_key
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP
 from cryptography.hazmat.primitives.asymmetric.padding import MGF1
 from cryptography.hazmat.primitives.hashes import SHA256
+from app.crud import leer_chat, insertar_mensaje
 import binascii
 import os
 import file_manager
 import sys
 
-def main():
+async def main():
     # Si es la primera ejecución, se crea y guarda un usuario
-    user = None
-    if not os.path.exists(file_manager.FICHERO_CONFIG):
-        print("PRIMERA EJECUCIÓN")
-        print("Creando usuario...")
-        user = inicializar_usuario()
-        print("\t\t\t[COMPLETADO]")
-        print("Guardando usuario...")
-        file_manager.guardar_usuario(user)
-        print("\t\t\t[COMPLETADO]")
-    else:
-        print("Cargando configuración...")
-        user = file_manager.leer_usuario()
-        print("\t\t\t[COMPLETADO]")
+    print("Cargando configuración...")
+    user = file_manager.leer_usuario()
+    print("\t\t\t[COMPLETADO]")
     
     print("Obteniendo IP del usuario...")
     ip = obtener_ip_privada()
@@ -37,33 +33,41 @@ def main():
     print("Actualizando TestNet...")
     contract = Contrato()
     contract.actualizar_ip(
-        binascii.hexlify(user.hash).decode('utf-8'), 
-        binascii.hexlify(ip_cifrada).decode('utf-8')
+        hash_to_string(user.hash), 
+        hash_to_string(ip_cifrada)
     )
     print("\t\t\t[COMPLETADO]")
 
-    # Si somos el usuario A, enviamos un mensaje al usuario B
-    if (sys.argv[1] == "user_a"):
-        # Recuperamos el chat de la Base de Datos
-        # COMPLETAR
+    # Recuperamos el chat de la Base de Datos
+    chat = leer_chat(open("resources/demo_chat.txt").read())
+    
+    # Obtenemos la info del contacto y lo añadimos al chat
+    hash_contacto = open("resources/hash_contacto.txt").read()
+    ip_contacto = contract.consultar_ip(hash_contacto)
+    chat.addMiembro(Contacto(chat.pub_key, ip_contacto, string_to_hash(hash_contacto)))
 
+    # Si somos el usuario A y no es la primera ejecución, enviamos un mensaje al usuario B
+    if (sys.argv[1] == "user_a"):
         # Creamos un mensaje
-        # COMPLETAR
+        message = Mensaje("texto_prueba", hash_to_string(chat.id_chat))
 
         # Enviamos el mensaje
-        # Completar -> Chat.enviar_mensaje()
-        pass
+        await chat.send_message(message)
+
     else:
         # Si no, esperamos a recibir un mensaje y lo mostramos
         read = False
+        mensajes = []
         while not read:
             # Verificamos si hay algún mensaje nuevo
-            # COMPLETAR
+            mensajes = chat.read_new_messages()
+            read = len(mensajes) > 0
 
             # Esperamos 1 segundo
             sleep(1)
         
         # Mostramos el mensaje leído
-        # COMPLETAR
+        for m in mensajes:
+            print(m.to_json())
 
-main()
+asyncio.run(main)
