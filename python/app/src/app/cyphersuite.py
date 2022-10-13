1554c8a2bf6d3919f18281882f49e41fe49f4817
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-10-13 13:18:48 +0200

    Cyphersuite con metodos adicionales

--- python/app/src/app/cyphersuite.py
+++ python/app/src/app/cyphersuite.py
@@ -6,6 +6,8 @@ from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP
 from cryptography.hazmat.primitives.asymmetric.padding import MGF1
 from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives import serialization 
+
 
 def cifrar_mensaje(mensaje: Mensaje, pub_key: RSAPublicKey) -> str:
     return binascii.hexlify(
@@ -34,4 +36,38 @@ def descifrar_mensaje(mensaje_cifrado: str, priv_key: RSAPrivateKey) -> Mensaje:
         texto=mensaje_descifrado['texto'],
         id_chat=mensaje_descifrado['id_chat'],
         ttl=mensaje_descifrado['ttl']
+    )
+
+def hash_to_string(hash: bytes) -> str:
+    return binascii.hexlify(hash).decode('utf-8')
+
+def string_to_hash(string: str) -> bytes:
+    return binascii.unhexlify(string.encode('utf-8'))
+
+def priv_key_to_string(key: RSAPrivateKey) -> str:
+    return binascii.hexlify(
+        key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    ).decode('UTF-8')
+
+def string_to_priv_key(string: str) -> RSAPrivateKey:
+    return serialization.load_pem_private_key(
+        binascii.unhexlify(string.encode('utf-8')),
+        None
+    )
+
+def pub_key_to_string(key: RSAPublicKey) -> str:
+    return binascii.hexlify(
+        key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    ).decode('UTF-8')
+
+def string_to_pub_key(string: str) -> RSAPublicKey:
+    return serialization.load_pem_public_key(
+        binascii.unhexlify(string.encode('utf-8'))
     )
--- python/app/tests/test_cyphersuite.py
+++ python/app/tests/test_cyphersuite.py
@@ -1,14 +1,24 @@
 import binascii
+import imp
 import json
 from app.chat import Chat
 from app.cyphersuite import cifrar_mensaje
 from app.cyphersuite import descifrar_mensaje
+from app.cyphersuite import hash_to_string
+from app.cyphersuite import string_to_hash
+from app.cyphersuite import priv_key_to_string
+from app.cyphersuite import string_to_priv_key
+from app.cyphersuite import pub_key_to_string
+from app.cyphersuite import string_to_pub_key
 from app.mensaje import Mensaje
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP
 from cryptography.hazmat.primitives.asymmetric.padding import MGF1
 from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from cryptography.hazmat.primitives import serialization 
 import pytest
 
 @pytest.fixture
@@ -56,3 +66,62 @@ def test_descifrar_mensaje(crear_chat: Chat):
 
     # Desciframos el mensaje y comprobamos si el texto es el mismo
     assert descifrar_mensaje(mensaje_cifrado, chat.priv_key).texto == mensaje.texto
+
+def test_hash_to_string(crear_chat: Chat):
+    chat = crear_chat
+    assert binascii.hexlify(chat.id_chat).decode('utf-8') == hash_to_string(chat.id_chat)
+
+def test_priv_key_to_sring():
+    priv_key = rsa.generate_private_key(65537, 2048)
+    priv_key_string = binascii.hexlify(
+        priv_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    ).decode('UTF-8')
+
+    assert priv_key_string == priv_key_to_string(priv_key)
+
+
+def test_pub_key_to_sring():
+    priv_key = rsa.generate_private_key(65537, 2048)
+    pub_key = priv_key.public_key()
+    pub_key_string = binascii.hexlify(
+        pub_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    ).decode('UTF-8')
+
+    assert pub_key_string == pub_key_to_string(pub_key)
+
+def test_string_to_hash(crear_chat: Chat):
+    # Obtenemos una representación string del hash de chat
+    chat = crear_chat
+    str_hash = binascii.hexlify(chat.id_chat).decode('utf-8')
+
+    # Verificamos que obtenemos id_chat a partir de su representación string
+    assert string_to_hash(str_hash) == chat.id_chat
+
+def test_string_to_priv_key(crear_chat: Chat):
+    chat = crear_chat
+    priv_key_string = binascii.hexlify(
+        chat.priv_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    ).decode('UTF-8')
+
+    assert chat.priv_key.key_size == string_to_priv_key(priv_key_string).key_size
+
+def test_string_to_pub_key(crear_chat: Chat):
+    chat = crear_chat
+    pub_key_string = binascii.hexlify(
+          chat.pub_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    ).decode('UTF-8')
+    assert chat.pub_key.key_size == string_to_pub_key(pub_key_string).key_size
