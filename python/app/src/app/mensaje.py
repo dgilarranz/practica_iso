d100c2c6310e2e092c40b6e6916914207913de27
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-11-14 14:33:09 +0100

    Mensaje -> id mensaje añadido para ser uuid, to_json actualizado para tenerlo en cuenta

--- python/app/src/app/mensaje.py
+++ python/app/src/app/mensaje.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import json
+import uuid
 
 class Mensaje:
-    def __init__(self, texto, id_chat, id_sender,ttl=None):
+    def __init__(self, texto, id_chat, id_sender, ttl=None):
         self.texto=texto
         self.ttl= ttl
         self.timestamp= datetime.now()
-        self.id_mensaje = None          # COMPLETAR
+        self.id_mensaje = uuid.uuid4()
         self.id_chat= id_chat
         self.id_sender = id_sender
     
@@ -15,7 +16,7 @@ class Mensaje:
             'texto': self.texto,
             'ttl': str(self.ttl),
             'timestamp': str(self.timestamp),
-            'id_mensaje': self.id_mensaje,
+            'id_mensaje': str(self.id_mensaje),
             'id_chat': self.id_chat,
             'id_sender': self.id_sender
         }
--- python/app/tests/test_mensaje.py
+++ python/app/tests/test_mensaje.py
@@ -1,9 +1,14 @@
 from email import message
 import json
 from app.mensaje import Mensaje
+from uuid import UUID
 
 def test_to_json():
     message = Mensaje("text", "id_chat", None)
     json_message = message.to_json()
 
     assert json.loads(json_message)['texto'] == 'text' 
+
+def test_message_has_uuid():
+    message = Mensaje("text", "id_chat", None)
+    assert isinstance(message.id_mensaje, UUID)
