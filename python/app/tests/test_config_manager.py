881b2d7d4e6723784b7ac46dd07aae54bbb05fb8
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-11-14 11:27:37 +0100

    ConfigManager -> tests que comprueban que almacena los atributos necesarios añadidos

--- python/app/tests/test_config_manager.py
+++ python/app/tests/test_config_manager.py
@@ -4,3 +4,9 @@ def test_cm_is_singleton():
     cm_instance_1 = ConfigManager()
     cm_instance_2 = ConfigManager()
     assert cm_instance_1 is cm_instance_2
+
+def test_cm_has_user_property():
+    assert hasattr(ConfigManager(), "user")
+
+def test_cm_hash_connection_manager_property():
+    assert hasattr(ConfigManager(), "connection_manager")
