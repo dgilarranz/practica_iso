4192ec80df828bde460e977f752f665e87496b7f
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2022-12-06 20:42:54 +0100

    MoneyFrame -> Tests para create_box, primer test para asegurar contenido

--- python/app/src/app/gui/money_frame.py
+++ python/app/src/app/gui/money_frame.py
@@ -1,4 +1,27 @@
 import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN
 
 class MoneyFrame(toga.Window):
-    pass
+    
+    def __init__(self):
+        super().__init__()
+
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+        from_box = self.create_box("From Address:")
+
+        join_button = toga.Button('Unirse al chat', on_press=None)
+        
+        main_box.add(from_box)
+        self.content = main_box
+
+
+
+    def create_box(self, label_text: str) -> toga.Box:
+        label = toga.Label(label_text, style=Pack(padding_left = 5, padding_right = 50))
+        input = toga.TextInput(style=Pack(flex=1))
+        
+        box = toga.Box(style=Pack(direction=COLUMN))
+        box.add(label)
+        box.add(input)
+        return box
--- python/app/tests/tests_gui/test_money_frame.py
+++ python/app/tests/tests_gui/test_money_frame.py
@@ -1,5 +1,54 @@
 from app.gui.money_frame import MoneyFrame
 import toga
+from functools import reduce
 
 def test_money_frame_is_toga_window():
     assert isinstance(MoneyFrame(), toga.Window)
+
+def test_create_box_creates_box_with_supplied_string_as_label():
+    mf = MoneyFrame()
+    text = "Prueba"
+    box = mf.create_box(text)
+    label = list(
+        filter(
+            lambda c: isinstance(c, toga.Label),
+            box.children
+        )
+    )[0]
+    assert label.text == text
+
+def test_create_box_creates_box_with_another_string_as_label():
+    mf = MoneyFrame()
+    text = "String diferente"
+    box = mf.create_box(text)
+    label = list(
+        filter(
+            lambda c: isinstance(c, toga.Label),
+            box.children
+        )
+    )[0]
+    assert label.text == text
+
+def test_create_box_also_creates_an_input():
+    mf = MoneyFrame()
+    box = mf.create_box("Prueba")
+    text_input = list(
+        filter(
+            lambda c: isinstance(c, toga.TextInput),
+            box.children
+        )
+    )[0]
+    assert text_input is not None
+
+def test_create_box_only_has_two_children():
+    mf = MoneyFrame()
+    box = mf.create_box("Prueba")
+    assert len(box.children) == 2
+
+def test_init_adds_to_content_from_address_box():
+    mf = MoneyFrame()
+    box = reduce(
+        lambda box, current: current if current.children[0].text == "From Address:" else box,
+        mf.content.children
+    )
+    assert box is not None
